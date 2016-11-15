0554e7f59bd28549ceca46bd01b0de518bc2c176
icasdri <icasdri@gmail.com>
2016-11-15 00:32:09 -0500

    Working compiled-in web assets (via compile-time code generation) -- we can now distribute everything as a single binary

--- build.rs
+++ build.rs
@@ -1,14 +1,16 @@
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 use std::path::Path;
 use std::process::Command;
 use std::process::ExitStatus;
 
-static asset_files: &'static [&'static str] = &[
+static ASSET_FILES: &'static [&'static str] = &[
     "node_modules/dygraphs/dygraph-combined.js",
     "node_modules/vue/dist/vue.min.js",
     "app.js",
-    "app.css",
+    "styles.css",
     "index.html",
 ];
 
@@ -28,10 +30,33 @@ fn main() {
     let assets_out_dir = out_dir.join("assets");
     fs::create_dir_all(&assets_out_dir).unwrap();
 
-    for asset_source in asset_files {
+    let mut wahb = String::new();
+    wahb.push_str("fn _webassets_handler_body<'a>(path: &'a str) -> Option<(&'static str, &'static str)> {\n");
+    wahb.push_str("match path {\n");
+
+    for asset_source in ASSET_FILES {
         let source_path = client_dir.join(asset_source);
         let asset_filename = source_path.file_name().unwrap();
         let target_path = assets_out_dir.join(asset_filename);
+        let content_type = match target_path.extension().unwrap().to_str().unwrap() {
+            "html" | "htm" => "text/html",
+            "js" => "application/javascript",
+            "css" => "text/css",
+            _ => "text/plain"
+        };
+        let match_arm = format!("\"{}\" => Some((include_str!(\"{}\"), \"{}\")),\n",
+                                asset_filename.to_str().unwrap(),
+                                target_path.to_str().unwrap(),
+                                content_type);
+        wahb.push_str(&match_arm);
         fs::copy(&source_path, &target_path).unwrap();
     }
+
+    wahb.push_str("_ => None\n");
+    wahb.push_str("}\n");
+    wahb.push_str("}\n");
+
+    let wa_handler_body_path = out_dir.join("webassets_handler_body.rs");
+    let mut wa_handler_body_file = File::create(&wa_handler_body_path).unwrap();
+    wa_handler_body_file.write_all(wahb.as_bytes()).unwrap();
 }
--- src/webserver.rs
+++ src/webserver.rs
@@ -1,11 +1,50 @@
 use std::thread;
 use std::path::Path;
+use std::error::Error;
+use std::fmt;
 
-use iron::prelude::{Request, Response, Chain, Iron, IronResult};
+use iron::prelude::{Request, Response, Chain, Iron, IronResult, IronError};
+use iron::headers::ContentType;
+use iron::mime::Mime;
+use iron::modifiers::Header;
 use iron::status;
 use router::Router;
 use mount::Mount;
 use staticfile::Static;
+
+#[derive(Debug)]
+struct NotFoundError;
+
+impl Error for NotFoundError {
+    fn description(&self) -> &str {
+        "Not found."
+    }
+}
+impl fmt::Display for NotFoundError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(self.description())
+    }
+}
+
+
+include!(concat!(env!("OUT_DIR"), "/webassets_handler_body.rs"));
+
+fn webassets_handler(req: &mut Request) -> IronResult<Response> {
+    let path = {
+        let p = req.url.path()[0];
+        if p.len() > 0 {
+            p
+        } else {
+            "index.html"
+        }
+    };
+
+    match _webassets_handler_body(path) {
+        Some((s, ct)) => Ok(Response::with(
+                (status::Ok, Header(ContentType(ct.parse().unwrap())), s))),
+        None => Err(IronError::new(NotFoundError, status::NotFound))
+    }
+}
 
 pub fn web_server(addr: String) -> thread::JoinHandle<()> {
     fn hello_handler(req: &mut Request) -> IronResult<Response> {
@@ -13,12 +52,12 @@ pub fn web_server(addr: String) -> thread::JoinHandle<()> {
     }
 
     let mut router = Router::new();
-    router.get("/", Static::new(Path::new("client/index.html")), "index");
+    router.get("/", webassets_handler, "index");
     router.get("/hello", hello_handler, "hello");
 
     let mut mount = Mount::new();
     mount.mount("/", router);
-    mount.mount("/assets/", Static::new(Path::new("client/")));
+    mount.mount("/assets/", webassets_handler);
 
     let iron = Iron::new(mount);
 
