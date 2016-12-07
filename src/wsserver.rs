2c06567c70eb501ed178d67ee108258af4cd0d78
icasdri <icasdri@gmail.com>
2016-12-07 11:27:57 -0500

    Simplified and commented wsserver.

--- src/wsserver.rs
+++ src/wsserver.rs
@@ -2,45 +2,21 @@ use std::thread;
 use std::sync::{Arc, Mutex, RwLock};
 
 use ws;
-use ws::{Factory, Handler, Settings, Builder, WebSocket, Handshake};
+use ws::{Settings, Builder};
 
 use options::MainConfiguration;
 
-struct ServerHandler {
-    out: ws::Sender,
-}
-
-impl Handler for ServerHandler {
-    fn on_open(&mut self, _: Handshake) -> ws::Result<()> {
-        println!("Websocket connection opened.");
-        Ok(())
-    }
-}
-
-struct ServerFactory;
-
-impl ServerFactory {
-    fn new() -> ServerFactory {
-        ServerFactory {}
-    }
-}
-
-impl Factory for ServerFactory {
-    type Handler = ServerHandler;
-
-    fn connection_made(&mut self, sender: ws::Sender) -> ServerHandler {
-        ServerHandler {
-            out: sender
-        }
-    }
-}
-
-
+/**
+ * Error container for websocket broadcasts.
+ */
 pub enum BroadcastError {
     SocketNotAvail,
     WebSocketError(ws::Error)
 }
 
+/**
+ * Wrapper around a websocket broadcast sender for sharing across threads.
+ */
 pub struct Broadcaster {
     sender: Mutex<Option<ws::Sender>>,
 }
@@ -52,11 +28,18 @@ impl Broadcaster {
         }
     }
 
+    /**
+     * Sets/updates the broadcast sender wrapped by this wrapper.
+     */
     fn update(&self, new_sender: ws::Sender) {
         let mut guard = self.sender.lock().unwrap();
         *guard = Some(new_sender);
     }
 
+    /**
+     * Broadcasts (sends through the broadcast sender) a message to all
+     * connected websocket clients.
+     */
     pub fn send<M>(&self, msg: M) -> Result<(), BroadcastError> where M: Into<ws::Message> {
         let guard = self.sender.lock().unwrap();
         if let Some(ref b) = *guard {
@@ -67,18 +50,24 @@ impl Broadcaster {
     }
 }
 
-fn get_socket() -> WebSocket<ServerFactory> {
-    let mut builder = Builder::new();
-    builder.with_settings(Settings::default());
-    builder.build(ServerFactory::new()).unwrap()
-}
-
 pub fn ws_server(configuration: Arc<RwLock<MainConfiguration>>,
                  broadcaster: Arc<Broadcaster>) -> thread::JoinHandle<()> {
     let ws_port = configuration.read().unwrap().ws_port;
     thread::spawn(move || {
         loop {
-            let socket = get_socket();
+            let socket = {
+                let mut builder = Builder::new();
+                builder.with_settings(Settings::default());
+                /*
+                 * build a socket with a dumb factory and handler, as we only
+                 * need the socket for the broadcaster
+                 */
+                builder.build(|_| {
+                    move |_| {
+                        Ok(())
+                    }
+                }).unwrap()
+            };
             broadcaster.update(socket.broadcaster());
             println!("WebSocket server (re)listening on port {}.", ws_port);
             socket.listen(("0.0.0.0", ws_port))
